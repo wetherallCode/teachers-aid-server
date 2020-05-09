ce5c632f338653869ee6c55f7d9c334d5f7e0953
Rob Wetherall <moosemacbook@Robs-MacBook-Pro.local>
2020-05-09 08:43:20 -0400

    changed title to textTitle

---
+++ src/types/texts/text.ts
@@ -0,0 +1,27 @@
+import { objectType, inputObjectType } from '@nexus/schema'
+import { Chapter } from '.'
+
+export const Text = objectType({
+  name: 'Text',
+  definition(t) {
+    t.id('_id', { nullable: true })
+    t.string('textTitle')
+    t.list.field('hasChapters', {
+      type: Chapter,
+      async resolve(parent, __, { textData }) {
+        const chapters = await textData
+          .find({ 'fromText.textTitle': parent.textTitle })
+          .toArray()
+        return chapters
+      },
+    })
+  },
+})
+
+export const TextInput = inputObjectType({
+  name: 'TextInput',
+  definition(t) {
+    t.string('textTitle', { required: true })
+    t.list.string('chapters', { required: true })
+  },
+})
