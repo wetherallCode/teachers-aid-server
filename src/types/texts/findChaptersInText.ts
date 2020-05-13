e4092268d42522f091c9d2f646167f5104d92a29
Rob Wetherall <moosemacbook@Robs-MacBook-Pro.local>
2020-05-13 16:18:54 -0400

    added findChaptersInText

---
+++ src/types/texts/findChaptersInText.ts
@@ -0,0 +1,28 @@
+import { objectType, inputObjectType, arg, queryField } from '@nexus/schema'
+import { Chapter } from '.'
+
+export const FindChaptersInTextInput = inputObjectType({
+  name: 'FindChaptersInTextInput',
+  definition(t) {
+    t.string('textTitle', { required: true })
+  },
+})
+
+export const FindChaptersInTextPayload = objectType({
+  name: 'FindChaptersInTextPayload',
+  definition(t) {
+    t.list.field('chapters', { type: Chapter })
+  },
+})
+
+export const FindChaptersInText = queryField('findChaptersInText', {
+  type: FindChaptersInTextPayload,
+  args: { input: arg({ type: FindChaptersInTextInput, required: true }) },
+  async resolve(_, { input: { textTitle } }, { textData }) {
+    const chapters = await textData
+      .find({ 'fromText.textTitle': textTitle })
+      .toArray()
+    console.log(chapters)
+    return { chapters }
+  },
+})
