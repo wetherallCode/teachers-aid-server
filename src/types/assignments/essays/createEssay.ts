f19d2ec67e0745b6bc829b5e9b43d59e9c95b060
Rob Wetherall <moosemacbook@Robs-MacBook-Pro.local>
2020-06-08 08:44:29 -0400

    modified createEssay

--- src/types/assignments/essays/createEssay.ts
+++ src/types/assignments/essays/createEssay.ts
@@ -9,13 +9,14 @@ export const CreateEssayInput = inputObjectType({
   definition(t) {
     t.field('topic', { type: TopicInput, required: true })
     t.field('readings', { type: ReadingsInput, required: true })
-    t.list.string('assignedCourseId', { required: true })
+    t.list.id('assignedCourseId', { required: true })
     t.id('associatedLessonId', { required: true })
     // t.list.string('hasOwner', { required: true })
     t.string('hasAssignerId', { required: true })
     t.int('maxPoints', { required: true })
     t.field('markingPeriod', { type: MarkingPeriodEnum, required: true })
     t.date('dueDate', { required: true })
+    t.date('assignedDate', { required: true })
   },
 })
 
@@ -41,65 +42,62 @@ export const CreateEssay = mutationField('createEssay', {
         maxPoints,
         markingPeriod,
         dueDate,
+        assignedDate,
       },
     },
     { assignmentData, userData }
   ) {
-    {
-      console.log(new Date().toISOString())
+    console.log(new Date().toISOString())
+    const beginningValue = [
+      {
+        type: 'paragraph',
+        children: [{ text: '' }],
+      },
+    ]
 
-      const studentList: NexusGenRootTypes['Student'][] = []
+    const assigner = await userData.findOne({
+      _id: new ObjectId(hasAssignerId),
+    })
 
-      for (const _id of assignedCourseId) {
-        const students = await userData
-          .find({
-            'inCourses._id': new ObjectId(_id),
-          })
-          .toArray()
-        students.forEach((student: NexusGenRootTypes['Student']) => {
-          studentList.push(student)
+    const studentList: NexusGenRootTypes['Student'][] = []
+    const newEssays: NexusGenRootTypes['Essay'][] = []
+
+    for (const _id of assignedCourseId) {
+      const students = await userData
+        .find({
+          'inCourses._id': new ObjectId(_id),
         })
-      }
-
-      const assigner = await userData.findOne({
-        _id: new ObjectId(hasAssignerId),
+        .toArray()
+      students.forEach((student: NexusGenRootTypes['Student']) => {
+        studentList.push(student)
       })
-
-      const newEssays: NexusGenRootTypes['Essay'][] = []
-
-      const beginningValue = [
-        {
-          type: 'paragraph',
-          children: [{ text: '' }],
-        },
-      ]
-
-      for (const student of studentList) {
-        const newEssay: NexusGenRootTypes['Essay'] = {
-          topic,
-          assiged: false,
-          assignedDate: new Date().toLocaleDateString(),
-          associatedLessonId,
-          dueDate,
-          readings,
-          workingDraft: {
-            draft: JSON.stringify(beginningValue),
-          },
-          markingPeriod,
-          hasAssigner: assigner,
-          hasOwner: await userData.findOne({
-            _id: new ObjectId(student._id!),
-          }),
-          score: { earnedPoints: 0, maxPoints },
-          late: true,
-          exempt: false,
-        }
-        const { insertedId } = await assignmentData.insertOne(newEssay)
-        newEssay._id = insertedId
-        newEssays.push(newEssay)
-      }
-      console.log(new Date().toISOString())
-      return { essays: newEssays }
     }
+
+    for (const student of studentList) {
+      const newEssay: NexusGenRootTypes['Essay'] = {
+        topic,
+        assigned: false,
+        assignedDate,
+        associatedLessonId,
+        dueDate,
+        readings,
+        workingDraft: {
+          draft: JSON.stringify(beginningValue),
+        },
+        markingPeriod,
+        hasAssigner: assigner,
+        hasOwner: await userData.findOne({
+          _id: new ObjectId(student._id!),
+        }),
+        score: { earnedPoints: 0, maxPoints },
+        late: true,
+        exempt: false,
+      }
+      const { insertedId } = await assignmentData.insertOne(newEssay)
+      newEssay._id = insertedId
+      newEssays.push(newEssay)
+    }
+    console.log(new Date().toISOString())
+    return { essays: newEssays }
   },
 })
