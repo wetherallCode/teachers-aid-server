73be4503953e56986323f711c19860cca9ab2710
Rob Wetherall <moosemacbook@Robs-MacBook-Pro.local>
2020-06-09 14:18:57 -0400

    modified createCourse

--- src/types/courses/createCourse.ts
+++ src/types/courses/createCourse.ts
@@ -1,11 +1,13 @@
 import { inputObjectType, objectType, arg, mutationField } from '@nexus/schema'
 import { Course } from '.'
 import { NexusGenRootTypes } from 'teachers-aid-server/src/teachers-aid-typegen'
+import { CourseTypeEnum } from './course'
 
 export const CreateCourseInput = inputObjectType({
   name: 'CreateCourseInput',
   definition(t) {
     t.string('name', { required: true })
+    t.field('courseType', { type: CourseTypeEnum, required: true })
   },
 })
 
@@ -16,16 +18,17 @@ export const CreateCoursePayload = objectType({
   },
 })
 
-// export const CreateCourse = mutationField('createCourse', {
-//   type: CreateCoursePayload,
-//   args: { input: arg({ type: CreateCourseInput, required: true }) },
-//   async resolve(_, { input: { name } }, { courseData }) {
-//     const newCourse: NexusGenRootTypes['Course'] = {
-//       name,
-//     }
+export const CreateCourse = mutationField('createCourse', {
+  type: CreateCoursePayload,
+  args: { input: arg({ type: CreateCourseInput, required: true }) },
+  async resolve(_, { input: { name, courseType } }, { courseData }) {
+    const newCourse: NexusGenRootTypes['Course'] = {
+      courseType,
+      name,
+    }
 
-//     const { insertedId } = await courseData.insertOne(newCourse)
-//     newCourse._id = insertedId
-//     return { course: newCourse }
-//   },
-// })
+    const { insertedId } = await courseData.insertOne(newCourse)
+    newCourse._id = insertedId
+    return { course: newCourse }
+  },
+})
