3574958e9d4ca2d51a8c8d27467222ddba261f55
Rob Wetherall <moosemacbook@Robs-MacBook-Pro.local>
2020-06-09 08:11:01 -0400

    added studentAbsences

---
+++ src/types/students/studentAbsence.ts
@@ -0,0 +1,13 @@
+import { objectType } from '@nexus/schema'
+import { MarkingPeriodEnum } from '../general'
+import { Student } from '.'
+
+export const StudentAbsence = objectType({
+  name: 'StudentAbsence',
+  definition(t) {
+    t.id('_id', { nullable: true })
+    t.field('student', { type: Student })
+    t.date('dayAbsent')
+    t.field('markingPeriod', { type: MarkingPeriodEnum })
+  },
+})
