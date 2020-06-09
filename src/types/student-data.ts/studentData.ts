0de62f783f31d169e0d210ed5b79183a64ff9f48
Rob Wetherall <moosemacbook@Robs-MacBook-Pro.local>
2020-06-09 11:03:30 -0400

    merged student-data into student file

--- src/types/student-data.ts/studentData.ts
+++
@@ -1,9 +0,0 @@
-import { interfaceType, objectType } from '@nexus/schema'
-
-export const StudentData = objectType({
-  name: 'StudentData',
-  definition(t) {
-    t.id('_id', { nullable: true })
-    // t.resolveType((course))
-  },
-})
