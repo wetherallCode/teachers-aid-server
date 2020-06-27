f003c21144fe432d458db15aaa0afaae2374bdec
Rob Wetherall <moosemacbook@Robs-MacBook-Pro.local>
2020-06-26 21:04:01 -0400

    added findEssaysToGradeByCourseId

---
+++ src/types/assignments/essays/findEssaysToGradeByCourseId.tsx
@@ -0,0 +1,33 @@
+import { objectType, inputObjectType, arg, queryField } from '@nexus/schema'
+import { Essay } from '.'
+import { NexusGenRootTypes } from 'teachers-aid-server/src/teachers-aid-typegen'
+import { ObjectId } from 'mongodb'
+
+export const FindEssaysToGradeByIdInput = inputObjectType({
+  name: 'FindEssaysToGradeByIdInput',
+  definition(t) {
+    t.id('courseId', { required: true })
+  },
+})
+
+export const FindEssaysToGradeByIdPayload = objectType({
+  name: 'FindEssaysToGradeByIdPayload',
+  definition(t) {
+    t.list.field('essays', { type: Essay })
+  },
+})
+
+export const FindEssaysToGradeById = queryField('findEssaysToGradeById', {
+  type: FindEssaysToGradeByIdPayload,
+  args: { input: arg({ type: FindEssaysToGradeByIdInput, required: true }) },
+  async resolve(_, { input: { teacherId } }, { assignmentData }) {
+    const essays: NexusGenRootTypes['Essay'][] = await assignmentData
+      .find({
+        'hasAssigner._id': new ObjectId(teacherId),
+        finalDraft: { $exists: true },
+      })
+      .toArray()
+
+    return { essays }
+  },
+})
