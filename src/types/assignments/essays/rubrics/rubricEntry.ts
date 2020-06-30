2007cd6471a1cd23e987221f48ae0a11257abc8f
Rob Wetherall <moosemacbook@Robs-MacBook-Pro.local>
2020-06-30 14:43:27 -0400

    added RubricEntryInput

--- src/types/assignments/essays/rubrics/rubricEntry.ts
+++ src/types/assignments/essays/rubrics/rubricEntry.ts
@@ -1,4 +1,4 @@
-import { objectType, enumType } from '@nexus/schema'
+import { objectType, enumType, inputObjectType } from '@nexus/schema'
 import { WritingLevelEnum } from '../../../students/progress-metrics/writingMetrics'
 
 export const RubricEntry = objectType({
@@ -11,6 +11,19 @@ export const RubricEntry = objectType({
     t.list.field('rubricWritingLevels', { type: WritingLevelEnum })
     // t.field('rubricWritingLevel', { type: WritingLevelEnum })
     // t.list.field('linkedWritingLevels', { type: WritingLevelEnum })
+  },
+})
+
+export const RubricEntryInput = inputObjectType({
+  name: 'RubricEntryInput',
+  definition(t) {
+    t.string('entry', { required: true })
+    t.int('score', { required: true })
+    t.field('rubricSection', { type: RubricSectionEnum, required: true })
+    t.list.field('rubricWritingLevels', {
+      type: WritingLevelEnum,
+      required: true,
+    })
   },
 })
 
