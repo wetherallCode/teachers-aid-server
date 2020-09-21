baa3e6f4b06c8989fa0cc2ae0e07f5f3e83f3387
Rob Wetherall <moosemacbook@Robs-MacBook-Pro.local>
2020-09-21 06:38:46 -0400

    changed import to relative

--- src/types/school-day/student-questions/findStudentQuestions.ts
+++ src/types/school-day/student-questions/findStudentQuestions.ts
@@ -1,6 +1,6 @@
 import { objectType, inputObjectType, arg, queryField } from '@nexus/schema'
 import { StudentQuestion } from '.'
-import { NexusGenRootTypes } from 'teachers-aid-server/src/teachers-aid-typegen'
+import { NexusGenRootTypes } from '../../../teachers-aid-typegen'
 import { ObjectId } from 'mongodb'
 
 export const FindStudentQuestionsInput = inputObjectType({
