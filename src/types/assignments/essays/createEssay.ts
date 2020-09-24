59eafdf74371eada0df1262254dabe62078ec662
Rob Wetherall <moosemacbook@Robs-MacBook-Pro.local>
2020-09-24 16:39:59 -0400

    fixed logic to prevent duplicate essays

--- src/types/assignments/essays/createEssay.ts
+++ src/types/assignments/essays/createEssay.ts
@@ -56,7 +56,7 @@ export const CreateEssay = mutationField('createEssay', {
     },
     { assignmentData, userData, studentData, courseData }
   ) {
-    console.log(new Date().toISOString().substring(18, 23))
+    console.log(new Date().toISOString().substring(16, 23))
 
     const beginningValue = [
       {
@@ -86,82 +86,90 @@ export const CreateEssay = mutationField('createEssay', {
     const newEssays: NexusGenRootTypes['Essay'][] = []
 
     for (const student of studentList) {
-      const studentCoursesIds = student.inCourses.map((course) => course._id)
-      const teacherCoursesIds = assigner.teachesCourses.map(
-        (course) => course._id
-      )
-      const courseList: string[] = []
-      const studentCourses: any = []
-      studentCoursesIds.forEach((id) => studentCourses.push(id?.toString()))
-      teacherCoursesIds.forEach((id) => {
-        if (studentCourses.includes(id?.toString())) {
-          courseList.push(id!)
-        }
+      const essayCheck = await assignmentData.findOne({
+        'hasOwner._id': student._id!,
+        associatedLessonId,
+        workingDraft: { $exists: true },
       })
-      const courseId = courseList[0]
-      const assignedCourseInfo: NexusGenRootTypes['CourseInfo'] = await courseData.findOne(
-        { 'course._id': courseId }
-      )
 
-      function assignedDueTime(time: string) {
-        if (time === 'BEFORE_SCHOOL') {
+      if (!essayCheck) {
+        const studentCoursesIds = student.inCourses.map((course) => course._id)
+        const teacherCoursesIds = assigner.teachesCourses.map(
+          (course) => course._id
+        )
+        const courseList: string[] = []
+        const studentCourses: any = []
+        studentCoursesIds.forEach((id) => studentCourses.push(id?.toString()))
+        teacherCoursesIds.forEach((id) => {
+          if (studentCourses.includes(id?.toString())) {
+            courseList.push(id!)
+          }
+        })
+        const courseId = courseList[0]
+        const assignedCourseInfo: NexusGenRootTypes['CourseInfo'] = await courseData.findOne(
+          { 'course._id': courseId }
+        )
+
+        function assignedDueTime(time: string) {
+          if (time === 'BEFORE_SCHOOL') {
+            return '8:00:00 AM'
+          }
+          if (time === 'BEFORE_CLASS') {
+            return assignedCourseInfo.startsAt
+          }
+          if (time === 'AFTER_CLASS') {
+            return assignedCourseInfo.endsAt
+          }
+          if (time === 'AFTER_SCHOOL') {
+            return '2:15:00 PM'
+          }
           return '8:00:00 AM'
         }
-        if (time === 'BEFORE_CLASS') {
-          return assignedCourseInfo.startsAt
+
+        const dueTimeForAssignment = assignedDueTime(dueTime)
+
+        const writingMetric: NexusGenRootTypes['WritingMetrics'] = await studentData.findOne(
+          {
+            'student._id': student._id,
+            overallWritingMetric: { $exists: true },
+          }
+        )
+
+        const individualTopic: NexusGenRootTypes['Topic'][] = topicList.filter(
+          (topic: NexusGenRootTypes['Topic']) =>
+            topic.writingLevel ===
+            writingMetric.overallWritingMetric.overallWritingLevel
+        )
+
+        const newEssay: NexusGenRootTypes['Essay'] = {
+          topic: individualTopic[getRandomInt(individualTopic.length)],
+          assigned: false,
+          dueTime: dueTimeForAssignment,
+          assignedDate,
+          associatedLessonId,
+          dueDate,
+          readings,
+          leveledUp: false,
+          paperBased: false,
+          workingDraft: {
+            draft: JSON.stringify(beginningValue),
+          },
+          markingPeriod,
+          hasAssigner: assigner,
+          hasOwner: await userData.findOne({
+            _id: new ObjectId(student._id!),
+          }),
+          score: { earnedPoints: 0, maxPoints },
+          late: true,
+          exempt: false,
         }
-        if (time === 'AFTER_CLASS') {
-          return assignedCourseInfo.endsAt
-        }
-        if (time === 'AFTER_SCHOOL') {
-          return '2:15:00 PM'
-        }
-        return '8:00:00 AM'
+        const { insertedId } = await assignmentData.insertOne(newEssay)
+        newEssay._id = insertedId
+        newEssays.push(newEssay)
       }
-
-      const dueTimeForAssignment = assignedDueTime(dueTime)
-
-      const writingMetric: NexusGenRootTypes['WritingMetrics'] = await studentData.findOne(
-        {
-          'student._id': student._id,
-          overallWritingMetric: { $exists: true },
-        }
-      )
-
-      const individualTopic: NexusGenRootTypes['Topic'][] = topicList.filter(
-        (topic: NexusGenRootTypes['Topic']) =>
-          topic.writingLevel ===
-          writingMetric.overallWritingMetric.overallWritingLevel
-      )
-
-      const newEssay: NexusGenRootTypes['Essay'] = {
-        topic: individualTopic[getRandomInt(individualTopic.length)],
-        assigned: false,
-        dueTime: dueTimeForAssignment,
-        assignedDate,
-        associatedLessonId,
-        dueDate,
-        readings,
-        leveledUp: false,
-        paperBased: false,
-        workingDraft: {
-          draft: JSON.stringify(beginningValue),
-        },
-        markingPeriod,
-        hasAssigner: assigner,
-        hasOwner: await userData.findOne({
-          _id: new ObjectId(student._id!),
-        }),
-        score: { earnedPoints: 0, maxPoints },
-        late: true,
-        exempt: false,
-      }
-      const { insertedId } = await assignmentData.insertOne(newEssay)
-      newEssay._id = insertedId
-      newEssays.push(newEssay)
     }
 
-    console.log(new Date().toISOString().substring(18, 23))
+    console.log(new Date().toISOString().substring(16, 23))
     return { essays: newEssays }
   },
 })
