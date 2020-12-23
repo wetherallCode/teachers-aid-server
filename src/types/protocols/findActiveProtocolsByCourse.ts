3d9c597c9f14b446568a6bf3d03bd017ffa35625
Rob Wetherall <robertgwetherall1@gmail.com>
2020-12-23 12:52:28 -0500

    added findActiveProtocolsByCourse

---
+++ src/types/protocols/findActiveProtocolsByCourse.ts
@@ -0,0 +1,36 @@
+import { objectType, inputObjectType, arg, queryField } from '@nexus/schema'
+import { ObjectId } from 'mongodb'
+import { Protocol } from '.'
+
+export const FindActiveProtocolsByCourseInput = inputObjectType({
+  name: 'FindActiveProtocolsByCourseInput',
+  definition(t) {
+    t.id('courseId', { required: true })
+  },
+})
+
+export const FindActiveProtocolsByCoursePayload = objectType({
+  name: 'FindActiveProtocolsByCoursePayload',
+  definition(t) {
+    t.list.field('protocols', { type: Protocol })
+  },
+})
+
+export const FindActiveProtocolsByCourse = queryField(
+  'findActiveProtocolsByCourse',
+  {
+    type: FindActiveProtocolsByCoursePayload,
+    args: {
+      input: arg({ type: FindActiveProtocolsByCourseInput, required: true }),
+    },
+    async resolve(_, { input: { courseId } }, { protocolData }) {
+      const protocols = await protocolData
+        .find({
+          'student.inCourses._id': new ObjectId(courseId),
+          isActive: true,
+        })
+        .toArray()
+      return { protocols }
+    },
+  }
+)
