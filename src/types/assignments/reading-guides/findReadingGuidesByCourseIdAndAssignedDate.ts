1e80db5c5995287685734bcd0abc320f8eca0faf
Rob Wetherall <robertgwetherall1@gmail.com>
2021-02-16 14:04:30 -0500

    added query

---
+++ src/types/assignments/reading-guides/findReadingGuidesByCourseIdAndAssignedDate.ts
@@ -0,0 +1,48 @@
+import { objectType, inputObjectType, arg, queryField } from '@nexus/schema'
+import { ObjectId } from 'mongodb'
+import { NexusGenRootTypes } from '../../../teachers-aid-typegen'
+import { ReadingGuide } from '.'
+
+export const FindReadingGuidesByCourseIdAndAssignedDateInput = inputObjectType({
+  name: 'FindReadingGuidesByCourseIdAndAssignedDateInput',
+  definition(t) {
+    t.id('courseId', { required: true })
+    t.string('assignedDate')
+  },
+})
+
+export const FindReadingGuidesByCourseIdAndAssignedDatePayload = objectType({
+  name: 'FindReadingGuidesByCourseIdAndAssignedDatePayload',
+  definition(t) {
+    t.list.field('readingGuides', { type: ReadingGuide })
+  },
+})
+
+export const FindReadingGuidesByCourseIdAndAssignedDate = queryField(
+  'findReadingGuidesByCourseIdAndAssignedDate',
+  {
+    type: FindReadingGuidesByCourseIdAndAssignedDatePayload,
+    args: {
+      input: arg({
+        type: FindReadingGuidesByCourseIdAndAssignedDateInput,
+        required: true,
+      }),
+    },
+    async resolve(
+      _,
+      { input: { courseId, assignedDate } },
+      { assignmentData }
+    ) {
+      const readingGuides: NexusGenRootTypes['ReadingGuide'][] = await assignmentData
+        .find({
+          'hasOwner.inCourses._id': new ObjectId(courseId),
+          assignedDate,
+          workingDraft: { $exists: false },
+          articleTitle: { $exists: false },
+        })
+        .toArray()
+
+      return { readingGuides }
+    },
+  }
+)
