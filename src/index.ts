6a566828e06ac50edababeb3d3739d9ab54564fe
Rob Wetherall <robertgwetherall1@gmail.com>
2021-05-05 14:08:15 -0400

    formatted

--- src/index.ts
+++ src/index.ts
@@ -10,119 +10,111 @@ import { makeSchema } from '@nexus/schema'
 import * as types from './types'
 
 const startServer = async () => {
-  const app = express()
+	const app = express()
 
-  const schema = makeSchema({
-    types,
-    outputs: {
-      schema: path.join(__dirname, '../teachers-aid.graphql'),
-      typegen: path.join(
-        __dirname.replace(/\/dist$/, '/src'),
-        '../src/teachers-aid-typegen.ts'
-      ),
-    },
-    typegenAutoConfig: {
-      sources: [
-        {
-          source: path.join(
-            __dirname.replace(/\/dist$/, '/src'),
-            './typeDefs.ts'
-          ),
-          alias: 't',
-        },
-      ],
-      contextType: 't.Context',
-    },
-  })
+	const schema = makeSchema({
+		types,
+		outputs: {
+			schema: path.join(__dirname, '../teachers-aid.graphql'),
+			typegen: path.join(__dirname.replace(/\/dist$/, '/src'), '../src/teachers-aid-typegen.ts'),
+		},
+		typegenAutoConfig: {
+			sources: [
+				{
+					source: path.join(__dirname.replace(/\/dist$/, '/src'), './typeDefs.ts'),
+					alias: 't',
+				},
+			],
+			contextType: 't.Context',
+		},
+	})
 
-  const store = new MongoDBStore({
-    uri: process.env.MONGO_SESSION_STORAGE,
-    collection: 'mySessions',
-  })
+	const store = new MongoDBStore({
+		uri: process.env.MONGO_SESSION_STORAGE,
+		collection: 'mySessions',
+	})
 
-  store.on('error', function (error: any) {
-    console.log('error in store: ' + error)
-  })
+	store.on('error', function (error: any) {
+		console.log('error in store: ' + error)
+	})
 
-  app.set('trust proxy', 1)
-  app.use(
-    session({
-      secret: process.env.SESSION_SECRET as string,
-      resave: false,
-      cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 7,
-        // maxAge: 1000 * 60 * 90,
-        httpOnly: true,
-        sameSite: 'none',
-        secure: true,
-      },
-      store: store,
-      saveUninitialized: false,
-    }) as any
-  )
-  // console.log('working')
-  const MONGO_DB = process.env.DB_HOST
-  const client = await MongoClient.connect(MONGO_DB as string, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  const db = client.db()
+	app.set('trust proxy', 1)
+	app.use(
+		session({
+			secret: process.env.SESSION_SECRET as string,
+			resave: false,
+			cookie: {
+				maxAge: 1000 * 60 * 60 * 24 * 7,
+				// maxAge: 1000 * 60 * 90,
+				// httpOnly: true,
+				// sameSite: 'none',
+				// secure: true,
+			},
+			store: store,
+			saveUninitialized: false,
+		}) as any
+	)
+	// console.log('working')
+	const MONGO_DB = process.env.DB_HOST
+	const client = await MongoClient.connect(MONGO_DB as string, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	const db = client.db()
 
-  const server = new ApolloServer({
-    schema,
-    introspection: true,
-    playground: true,
-    engine: {
-      apiKey: 'service:teachers-aid-server:AVsJ7hHBz2dGu9JYONV84g',
-    },
-    context: async ({ req, res }: any) => {
-      let userData = db.collection('userData')
-      let assignmentData = db.collection('assignmentData')
-      let courseData = db.collection('courseData')
-      let textSectionData = db.collection('textSectionData')
-      let textData = db.collection('textData')
-      let lessonData = db.collection('lessonData')
-      let generalData = db.collection('generalData')
-      let studentData = db.collection('studentData')
-      let teacherData = db.collection('teacherData')
-      let rubricData = db.collection('rubricData')
-      let protocolData = db.collection('protocolData')
-      let schoolDayData = db.collection('schoolDayData')
-      let temporaryTaskData = db.collection('temporaryTaskData')
+	const server = new ApolloServer({
+		schema,
+		introspection: true,
+		playground: true,
+		engine: {
+			apiKey: 'service:teachers-aid-server:AVsJ7hHBz2dGu9JYONV84g',
+		},
+		context: async ({ req, res }: any) => {
+			let userData = db.collection('userData')
+			let assignmentData = db.collection('assignmentData')
+			let courseData = db.collection('courseData')
+			let textSectionData = db.collection('textSectionData')
+			let textData = db.collection('textData')
+			let lessonData = db.collection('lessonData')
+			let generalData = db.collection('generalData')
+			let studentData = db.collection('studentData')
+			let teacherData = db.collection('teacherData')
+			let rubricData = db.collection('rubricData')
+			let protocolData = db.collection('protocolData')
+			let schoolDayData = db.collection('schoolDayData')
+			let temporaryTaskData = db.collection('temporaryTaskData')
 
-      return {
-        req,
-        res,
-        userData,
-        assignmentData,
-        studentData,
-        teacherData,
-        courseData,
-        textSectionData,
-        textData,
-        lessonData,
-        generalData,
-        rubricData,
-        protocolData,
-        schoolDayData,
-        temporaryTaskData,
-        db,
-      }
-    },
-  })
+			return {
+				req,
+				res,
+				userData,
+				assignmentData,
+				studentData,
+				teacherData,
+				courseData,
+				textSectionData,
+				textData,
+				lessonData,
+				generalData,
+				rubricData,
+				protocolData,
+				schoolDayData,
+				temporaryTaskData,
+				db,
+			}
+		},
+	})
 
-  server.applyMiddleware({
-    app,
-    cors: {
-      credentials: true,
-      origin: ['https://mrwetherall.org', 'http://localhost:3000'],
-    },
-  })
-  const PORT = process.env.PORT || 4000
+	server.applyMiddleware({
+		app,
+		cors: {
+			credentials: true,
+			origin: ['https://mrwetherall.org', 'http://localhost:3000'],
+		},
+	})
+	const PORT = process.env.PORT || 4000
 
-  app.listen({ port: PORT }, () =>
-    console.log(`🚀 Server ready at${server.graphqlPath}`)
-  )
+	app.listen({ port: PORT }, () => console.log(`🚀 Server ready at${server.graphqlPath}`))
 }
 
 startServer()
