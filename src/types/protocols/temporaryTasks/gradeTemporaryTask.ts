c54eef6d44510288e8a5993384c75ff90df37624
Rob Wetherall <robertgwetherall1@gmail.com>
2021-05-06 06:35:58 -0400

    deleted logs

--- src/types/protocols/temporaryTasks/gradeTemporaryTask.ts
+++ src/types/protocols/temporaryTasks/gradeTemporaryTask.ts
@@ -4,83 +4,89 @@ import { TemporaryTask } from '.'
 import { NexusGenRootTypes } from '../../../teachers-aid-typegen'
 
 export const GradeTemporaryTaskInput = inputObjectType({
-	name: 'GradeTemporaryTaskInput',
-	definition(t) {
-		t.id('_id', { required: true })
-		t.boolean('answered', { required: true })
-		t.float('responsibilityPoints', { required: true })
-		t.float('lastGrade', { required: true })
-	},
+  name: 'GradeTemporaryTaskInput',
+  definition(t) {
+    t.id('_id', { required: true })
+    t.boolean('answered', { required: true })
+    t.float('responsibilityPoints', { required: true })
+    t.float('lastGrade', { required: true })
+  },
 })
 
 export const GradeTemporaryTaskPayload = objectType({
-	name: 'GradeTemporaryTaskPayload',
-	definition(t) {
-		t.field('temporaryTask', { type: TemporaryTask })
-	},
+  name: 'GradeTemporaryTaskPayload',
+  definition(t) {
+    t.field('temporaryTask', { type: TemporaryTask })
+  },
 })
 
 export const GradeTemporaryTask = mutationField('gradeTemporaryTask', {
-	type: GradeTemporaryTaskPayload,
-	args: { input: arg({ type: GradeTemporaryTaskInput, required: true }) },
-	async resolve(
-		_,
-		{ input: { _id, answered, responsibilityPoints, lastGrade } },
-		{ temporaryTaskData, generalData, studentData }
-	) {
-		console.log(new Date().toLocaleTimeString())
-		const temporaryTaskCheck: NexusGenRootTypes['TemporaryTask'] = await temporaryTaskData.findOne({
-			_id: new ObjectId(_id),
-		})
-		// console.log(lastGrade)
-		const mp = await generalData.findOne({
-			currentMarkingPeriod: { $exists: true },
-		})
+  type: GradeTemporaryTaskPayload,
+  args: { input: arg({ type: GradeTemporaryTaskInput, required: true }) },
+  async resolve(
+    _,
+    { input: { _id, answered, responsibilityPoints, lastGrade } },
+    { temporaryTaskData, generalData, studentData }
+  ) {
+    const temporaryTaskCheck: NexusGenRootTypes['TemporaryTask'] = await temporaryTaskData.findOne(
+      {
+        _id: new ObjectId(_id),
+      }
+    )
+    // console.log(lastGrade)
+    const mp = await generalData.findOne({
+      currentMarkingPeriod: { $exists: true },
+    })
 
-		if (temporaryTaskCheck) {
-			const studentId = temporaryTaskCheck.student._id!
-			await temporaryTaskData.updateOne(
-				{ _id: new ObjectId(_id) },
-				{
-					$set: {
-						answered,
-						lastGrade: temporaryTaskCheck.answered && !answered ? 0 : responsibilityPoints,
-					},
-				}
-			)
-			// if already earned points for answering but mistake was made
-			if (temporaryTaskCheck.answered && !answered) {
-				await studentData.updateOne(
-					{
-						'student._id': new ObjectId(studentId!),
-						markingPeriod: mp.currentMarkingPeriod,
-						responsibilityPoints: { $exists: true },
-					},
-					{
-						$inc: { responsibilityPoints: -lastGrade },
-					}
-				)
-			}
+    if (temporaryTaskCheck) {
+      const studentId = temporaryTaskCheck.student._id!
+      await temporaryTaskData.updateOne(
+        { _id: new ObjectId(_id) },
+        {
+          $set: {
+            answered,
+            lastGrade:
+              temporaryTaskCheck.answered && !answered
+                ? 0
+                : responsibilityPoints,
+          },
+        }
+      )
+      // if already earned points for answering but mistake was made
+      if (temporaryTaskCheck.answered && !answered) {
+        await studentData.updateOne(
+          {
+            'student._id': new ObjectId(studentId!),
+            markingPeriod: mp.currentMarkingPeriod,
+            responsibilityPoints: { $exists: true },
+          },
+          {
+            $inc: { responsibilityPoints: -lastGrade },
+          }
+        )
+      }
 
-			//   if student hasn't already answered this task then they will get 2 responsibility points if they answer
-			if (!temporaryTaskCheck.answered && answered) {
-				await studentData.updateOne(
-					{
-						'student._id': new ObjectId(studentId!),
-						markingPeriod: mp.currentMarkingPeriod,
-						responsibilityPoints: { $exists: true },
-					},
-					{
-						$inc: { responsibilityPoints: responsibilityPoints },
-					}
-				)
-			}
+      //   if student hasn't already answered this task then they will get 2 responsibility points if they answer
+      if (!temporaryTaskCheck.answered && answered) {
+        await studentData.updateOne(
+          {
+            'student._id': new ObjectId(studentId!),
+            markingPeriod: mp.currentMarkingPeriod,
+            responsibilityPoints: { $exists: true },
+          },
+          {
+            $inc: { responsibilityPoints: responsibilityPoints },
+          }
+        )
+      }
 
-			const temporaryTask: NexusGenRootTypes['TemporaryTask'] = await temporaryTaskData.findOne({
-				_id: new ObjectId(_id),
-			})
-			console.log(new Date().toLocaleTimeString())
-			return { temporaryTask }
-		} else throw new Error('Task not created')
-	},
+      const temporaryTask: NexusGenRootTypes['TemporaryTask'] = await temporaryTaskData.findOne(
+        {
+          _id: new ObjectId(_id),
+        }
+      )
+
+      return { temporaryTask }
+    } else throw new Error('Task not created')
+  },
 })
