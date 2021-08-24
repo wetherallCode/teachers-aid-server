8757dfb00dcdd2461c20e647832eaf7bb66b8a9d
Rob Wetherall <robertgwetherall1@gmail.com>
2021-08-24 15:09:17 -0400

    added deleteAllProtocols

---
+++ src/types/protocols/deleteAllProtocols.ts
@@ -0,0 +1,20 @@
+import { objectType, inputObjectType, arg, mutationField } from '@nexus/schema'
+
+export const DeleteAllProtocolsPayload = objectType({
+  name: 'DeleteAllProtocolsPayload',
+  definition(t) {
+    t.boolean('removed')
+  },
+})
+
+export const DeleteAllProtocols = mutationField('deleteAllProtocols', {
+  type: DeleteAllProtocolsPayload,
+  async resolve(_, __, { protocolData }) {
+    const { deletedCount } = await protocolData.deleteMany()
+    console.log(deletedCount)
+    // if (deletedCount === 1) {
+    //   return { removed: true }
+    // }
+    return { removed: true }
+  },
+})
