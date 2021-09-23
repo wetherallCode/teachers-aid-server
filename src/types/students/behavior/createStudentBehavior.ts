d1badf92f507cbfbc5675ef0c14160df88f2d42c
Rob Wetherall <robertgwetherall1@gmail.com>
2021-09-23 14:10:48 -0400

    student check didn't have a new ObjectId

--- src/types/students/behavior/createStudentBehavior.ts
+++ src/types/students/behavior/createStudentBehavior.ts
@@ -5,63 +5,57 @@ import { ObjectId } from 'mongodb'
 import { MarkingPeriodEnum } from '../..'
 
 export const CreateStudentBehaviorInput = inputObjectType({
-  name: 'CreateStudentBehaviorInput',
-  definition(t) {
-    t.id('studentId', { required: true })
-    t.field('studentBehaviorType', { type: BehaviorEnum, required: true })
-    t.field('markingPeriod', { type: MarkingPeriodEnum, required: true })
-    t.float('responsibilityPoints', { required: true })
-  },
+	name: 'CreateStudentBehaviorInput',
+	definition(t) {
+		t.id('studentId', { required: true })
+		t.field('studentBehaviorType', { type: BehaviorEnum, required: true })
+		t.field('markingPeriod', { type: MarkingPeriodEnum, required: true })
+		t.float('responsibilityPoints', { required: true })
+	},
 })
 
 export const CreateStudentBehaviorPayload = objectType({
-  name: 'CreateStudentBehaviorPayload',
-  definition(t) {
-    t.field('studentBehavior', { type: StudentBehavior })
-  },
+	name: 'CreateStudentBehaviorPayload',
+	definition(t) {
+		t.field('studentBehavior', { type: StudentBehavior })
+	},
 })
 
 export const CreateStudentBehavior = mutationField('createStudentBehavior', {
-  type: CreateStudentBehaviorPayload,
-  args: { input: arg({ type: CreateStudentBehaviorInput, required: true }) },
-  async resolve(
-    _,
-    {
-      input: {
-        studentId,
-        studentBehaviorType,
-        markingPeriod,
-        responsibilityPoints,
-      },
-    },
-    { studentData, userData }
-  ) {
-    const studentCheck: NexusGenRootTypes['Student'] = await userData.findOne({
-      _id: studentId,
-    })
-    if (studentCheck) {
-      const studentBehavior: NexusGenRootTypes['StudentBehavior'] = {
-        behavior: studentBehaviorType,
-        date: new Date().toLocaleDateString(),
-        student: studentCheck,
-        responsibilityPoints,
-      }
-      const { insertedId } = await studentData.insertOne(studentBehavior)
-      studentBehavior._id = insertedId
+	type: CreateStudentBehaviorPayload,
+	args: { input: arg({ type: CreateStudentBehaviorInput, required: true }) },
+	async resolve(
+		_,
+		{ input: { studentId, studentBehaviorType, markingPeriod, responsibilityPoints } },
+		{ studentData, userData }
+	) {
+		const studentCheck: NexusGenRootTypes['Student'] = await userData.findOne({
+			_id: new ObjectId(studentId),
+		})
 
-      await studentData.updateOne(
-        {
-          'student._id': new ObjectId(studentId),
-          markingPeriod: markingPeriod,
-          responsibilityPoints: { $exists: true },
-        },
-        {
-          $inc: {
-            responsibilityPoints,
-          },
-        }
-      )
-      return { studentBehavior }
-    } else throw new Error('Student does not exist.')
-  },
+		if (studentCheck) {
+			const studentBehavior: NexusGenRootTypes['StudentBehavior'] = {
+				behavior: studentBehaviorType,
+				date: new Date().toLocaleDateString(),
+				student: studentCheck,
+				responsibilityPoints,
+			}
+			const { insertedId } = await studentData.insertOne(studentBehavior)
+			studentBehavior._id = insertedId
+
+			await studentData.updateOne(
+				{
+					'student._id': new ObjectId(studentId),
+					markingPeriod: markingPeriod,
+					responsibilityPoints: { $exists: true },
+				},
+				{
+					$inc: {
+						responsibilityPoints,
+					},
+				}
+			)
+			return { studentBehavior }
+		} else throw new Error('Student does not exist.')
+	},
 })
