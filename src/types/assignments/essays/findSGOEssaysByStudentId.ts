9e382d0a3815d7af1a7658af90c104678e58d967
Rob Wetherall <robertgwetherall1@gmail.com>
2022-04-18 08:20:43 -0400

    added query findSGOEssaysByStudentId

---
+++ src/types/assignments/essays/findSGOEssaysByStudentId.ts
@@ -0,0 +1,40 @@
+import { objectType, inputObjectType, arg, queryField } from '@nexus/schema'
+import { ObjectId } from 'mongodb'
+import { NexusGenRootTypes } from '../../../teachers-aid-typegen'
+import { Essay } from './essays'
+
+export const FindSGOEssaysByStudentIdInput = inputObjectType({
+  name: 'FindSGOEssaysByStudentIdInput',
+  definition(t) {
+    t.id('studentId', { required: true })
+  },
+})
+
+export const FindSGOEssaysByStudentIdPayload = objectType({
+  name: 'FindSGOEssaysByStudentIdPayload',
+  definition(t) {
+    t.list.field('essays', { type: Essay })
+  },
+})
+
+export const FindSGOEssaysByStudentId = queryField('findSGOEssaysByStudentId', {
+  type: FindSGOEssaysByStudentIdPayload,
+  args: { input: arg({ type: FindSGOEssaysByStudentIdInput, required: true }) },
+  async resolve(_, { input: { studentId } }, { assignmentData, questionData }) {
+    const essaysToSearch: NexusGenRootTypes['Essay'][] = await assignmentData
+      .find({
+        'hasOwner._id': new ObjectId(studentId),
+        workingDraft: { $exists: true },
+      })
+      .toArray()
+    const essays = []
+    for (const essay of essaysToSearch) {
+      const sgoEssayCheck = await questionData.findOne({
+        _id: new ObjectId(essay.topic.essayQuestionId),
+        sgoQuestion: true,
+      })
+      if (sgoEssayCheck) essays.push(essay)
+    }
+    return { essays }
+  },
+})
