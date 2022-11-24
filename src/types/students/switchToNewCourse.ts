995268490b083f57cd474810bfcf698ca2c5ee82
Rob Wetherall <robertgwetherall1@gmail.com>
2022-11-24 06:55:50 -0500

    fixed

--- src/types/students/switchToNewCourse.ts
+++ src/types/students/switchToNewCourse.ts
@@ -4,72 +4,86 @@ import { NexusGenRootTypes } from '../../teachers-aid-typegen'
 import { Student } from './student'
 
 export const SwitchToNewCourseInput = inputObjectType({
-	name: 'SwitchToNewCourseInput',
-	definition(t) {
-		t.id('studentId', { required: true })
-		t.id('newCourseId', { required: true })
-	},
+  name: 'SwitchToNewCourseInput',
+  definition(t) {
+    t.id('studentId', { required: true })
+    t.id('newCourseId', { required: true })
+    t.id('oldCourseId', { required: true })
+  },
 })
 
 export const SwitchToNewCoursePayload = objectType({
-	name: 'SwitchToNewCoursePayload',
-	definition(t) {
-		t.field('student', { type: Student })
-	},
+  name: 'SwitchToNewCoursePayload',
+  definition(t) {
+    t.field('student', { type: Student })
+  },
 })
 
 export const SwitchToNewCourse = mutationField('switchToNewCourse', {
-	type: SwitchToNewCoursePayload,
-	args: { input: arg({ type: SwitchToNewCourseInput, required: true }) },
-	async resolve(
-		_,
-		{ input: { studentId, newCourseId } },
-		{ studentData, courseData, assignmentData, protocolData, userData }
-	) {
-		const newCourse: NexusGenRootTypes['Course'] = await courseData.findOne({
-			_id: new ObjectId(newCourseId),
-		})
+  type: SwitchToNewCoursePayload,
+  args: { input: arg({ type: SwitchToNewCourseInput, required: true }) },
+  async resolve(
+    _,
+    { input: { studentId, newCourseId, oldCourseId } },
+    { studentData, courseData, assignmentData, protocolData, userData }
+  ) {
+    const newCourse: NexusGenRootTypes['Course'] = await courseData.findOne({
+      _id: new ObjectId(newCourseId),
+    })
+    const oldCourse: NexusGenRootTypes['Course'] = await courseData.findOne({
+      _id: new ObjectId(oldCourseId),
+    })
 
-		const studentToFind: NexusGenRootTypes['Student'] = await userData.findOne({
-			_id: new ObjectId(studentId),
-		})
-		if (studentToFind) {
-			const { name } = studentToFind.inCourses[0]!
+    const studentToFind: NexusGenRootTypes['Student'] = await userData.findOne({
+      _id: new ObjectId(studentId),
+    })
+    if (studentToFind) {
+      const { name } = studentToFind.inCourses[0]!
 
-			const { modifiedCount: assignmentCount } = await assignmentData.updateMany(
-				{
-					'hasOwner._id': new ObjectId(studentId),
-					'hasOwner.inCourses': studentToFind.inCourses[0],
-				},
-				{ $set: { 'hasOwner.inCourses.0': newCourse } }
-			)
-			console.log('assignmentCount, ' + assignmentCount)
-			const { modifiedCount: protocolCount } = await protocolData.updateMany(
-				{ 'student._id': new ObjectId(studentId), 'student.inCourses': studentToFind.inCourses[0] },
-				{ $set: { 'student.inCourses.0': newCourse } }
-			)
-			console.log('protocolCount, ' + protocolCount)
-			const { modifiedCount: studentDataCount } = await studentData.updateMany(
-				{ 'student._id': new ObjectId(studentId), 'student.inCourses': studentToFind.inCourses[0] },
-				{ $set: { 'student.inCourses.0': newCourse } }
-			)
-			console.log('studentDataCount, ' + studentDataCount)
-			const { modifiedCount: studentDataMetricsCount } = await studentData.updateMany(
-				{
-					'student._id': new ObjectId(studentId),
-					inCourse: { $exists: true },
-					// 'student.inCourses': studentToFind.inCourses[0],
-				},
-				{ $set: { 'inCourses.0': newCourse } }
-			)
-			console.log('studentDataMetricsCount, ' + studentDataMetricsCount)
-			await userData.updateMany(
-				{ _id: new ObjectId(studentId), inCourses: studentToFind.inCourses[0] },
-				{ $set: { 'inCourses.0': newCourse } }
-			)
-			const student = await userData.findOne({ _id: new ObjectId(studentId) })
-			return { student }
-		}
-		throw new Error('wait')
-	},
+      const { modifiedCount: assignmentCount } =
+        await assignmentData.updateMany(
+          {
+            'hasOwner._id': new ObjectId(studentId),
+            'hasOwner.inCourses': oldCourse,
+          },
+          { $set: { 'hasOwner.inCourses.$': newCourse } }
+        )
+      console.log('assignmentCount, ' + assignmentCount)
+      const { modifiedCount: protocolCount } = await protocolData.updateMany(
+        {
+          'student._id': new ObjectId(studentId),
+          'student.inCourses': oldCourse,
+        },
+        { $set: { 'student.inCourses.$': newCourse } }
+      )
+      console.log('protocolCount, ' + protocolCount)
+      const { modifiedCount: studentDataCount } = await studentData.updateMany(
+        {
+          'student._id': new ObjectId(studentId),
+          'student.inCourses': oldCourse,
+        },
+        { $set: { 'student.inCourses.$': newCourse } }
+      )
+      console.log('studentDataCount, ' + studentDataCount)
+
+      const { modifiedCount: studentDataMetricsCount } =
+        await studentData.updateMany(
+          {
+            'student._id': new ObjectId(studentId),
+            inCourse: { $exists: true },
+            // 'student.inCourses':oldCourse,
+          },
+          { $set: { inCourse: newCourse } }
+        )
+      console.log('studentDataMetricsCount, ' + studentDataMetricsCount)
+
+      await userData.updateMany(
+        { _id: new ObjectId(studentId) },
+        { $set: { 'inCourses.0': newCourse } }
+      )
+      const student = await userData.findOne({ _id: new ObjectId(studentId) })
+      return { student }
+    }
+    throw new Error('wait')
+  },
 })
